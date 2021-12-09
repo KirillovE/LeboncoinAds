ca1c0f821b2484ebd3df9d2c26bafc707aa0631c
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-09 13:41:35 +0100

    Converting single ad

--- ModelConverter/Sources/ModelConverter/ModelConverter.swift
+++ ModelConverter/Sources/ModelConverter/ModelConverter.swift
@@ -17,4 +17,32 @@ extension ModelConverter {
         }
     }
     
+    func convertSingle(_ ad: ClassifiedAd, using categories: [Int: String]) -> AdComplete? {
+        guard let categoryName = categories[ad.categoryId] else { return nil }
+
+        let summary = AdSummary(
+            title: ad.title,
+            categoryName: categoryName,
+            isUrgent: ad.isUrgent,
+            imageAddress: ad.imagesUrl.small,
+            price: ad.price
+        )
+
+        let details = AdDetails(
+            title: ad.title,
+            description: ad.description,
+            categoryName: categoryName,
+            creationDate: ad.creationDate,
+            isUrgent: ad.isUrgent,
+            imageAddress: ad.imagesUrl.thumb,
+            price: ad.price
+        )
+        
+        return .init(
+            id: ad.id,
+            categoryId: ad.categoryId,
+            summary: summary,
+            details: details
+        )
+    }
 }
--- ModelConverter/Tests/ModelConverterTests/ModelConverterTests.swift
+++ ModelConverter/Tests/ModelConverterTests/ModelConverterTests.swift
@@ -10,17 +10,58 @@ final class ModelConverterTests: XCTestCase {
         converter = .init()
     }
     
-    func testCategoriesConversion() {
-        let categories: [Models.Category] = [
+    func testFilledCategoriesConversion() {
+        let filledCategories: [Models.Category] = [
             .init(id: 1, name: "first"),
             .init(id: 2, name: "second")
         ]
-        let converted = converter.convertToDict(categories)
+        let converted = converter.convertToDict(filledCategories)
         
-        XCTAssertEqual(converted.count, categories.count)
-        categories.forEach { category in
+        XCTAssertEqual(converted.count, filledCategories.count)
+        filledCategories.forEach { category in
             XCTAssertNotNil(converted[category.id])
             converted[category.id].map { XCTAssertEqual($0, category.name) }
         }
+    }
+    
+    func testEmptyCategoriesConversion() {
+        let emptyCategories = [Models.Category]()
+        let converted = converter.convertToDict(emptyCategories)
+        XCTAssertTrue(converted.isEmpty)
+    }
+    
+    func testAdConversionSuccess() {
+        let categoryId = 1
+        let categoryName = "first"
+        let ad = makeAd(withCategoryID: categoryId)
+        let categories = [categoryId: categoryName]
+        let converted = converter.convertSingle(ad, using: categories)
+        
+        XCTAssertNotNil(converted)
+        converted.map { adComplete in
+            XCTAssertEqual(adComplete.summary.categoryName, categoryName)
+            XCTAssertEqual(adComplete.details.categoryName, categoryName)
+        }
+    }
+    
+    func testAdConversionFailure() {
+        let ad = makeAd(withCategoryID: 0)
+        let categories = [Int: String]()
+        let converted = converter.convertSingle(ad, using: categories)
+        
+        XCTAssertNil(converted)
+    }
+    
+    private func makeAd(withCategoryID id: Int) -> ClassifiedAd {
+        .init(
+            id: 0,
+            categoryId: id,
+            creationDate: "",
+            title: "",
+            description: "",
+            isUrgent: true,
+            imagesUrl: .init(small: "", thumb: ""),
+            price: 0
+        )
     }
 }
