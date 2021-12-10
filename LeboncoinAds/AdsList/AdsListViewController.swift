f8cef323de7c860b79a8b928cc593873a98641b6
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-10 22:03:54 +0100

    Configuring table with diffable data source

--- LeboncoinAds/AdsList/AdsListViewController.swift
+++ LeboncoinAds/AdsList/AdsListViewController.swift
@@ -4,7 +4,10 @@ import Models
 final class AdsListViewController: UIViewController {
     
     private let table = AdsListView()
+    private var dataSource: UITableViewDiffableDataSource<AdsListSection, AdComplete>?
+    private var currentSnapshot: NSDiffableDataSourceSnapshot<AdsListSection, AdComplete>?
     private var adsProvider: AdsProvider
+    
     private var allAds = [AdComplete]() {
         didSet {
             print("Received \(allAds.count) new ads")
@@ -24,16 +27,51 @@ final class AdsListViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func loadView() {
-        view = table
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         adsProvider.fetchAds()
+        configureTable()
+        configureDataSource()
     }
     
     private func handleError(_ error: TextualError) {
         print("Received error: \(error)")
     }
+    
+    private func configureTable() {
+        view.addSubview(table)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            table.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            table.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            table.topAnchor.constraint(equalTo: view.topAnchor),
+            table.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        table.configure(delegate: self)
+    }
+    
+    private func configureDataSource() {
+        let urgencySymbol = UIImage(systemName: "exclamationmark.circle")
+        
+        dataSource = UITableViewDiffableDataSource<AdsListSection, AdComplete>(tableView: table) {
+            [weak self] tableView, indexPath, itemIdentifier in
+            
+            guard let self = self else { return nil }
+ 
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.table.reuseID,for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            content.text = itemIdentifier.summary.title + "\n" + String(itemIdentifier.summary.price)
+            content.secondaryText = itemIdentifier.summary.categoryName
+//            content.image =
+            content.imageProperties.cornerRadius = (content.image?.size.height ?? 0) / 2
+            cell.accessoryView = itemIdentifier.summary.isUrgent ? UIImageView(image: urgencySymbol) : nil
+            cell.contentConfiguration = content
+            
+            return cell
+        }
+    }
+}
+
+extension AdsListViewController: UITableViewDelegate {
+    
 }
