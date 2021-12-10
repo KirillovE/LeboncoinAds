d792398b6c896f0921b608291f66728066f88893
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-10 22:03:18 +0100

    Adjusting table configuration

--- LeboncoinAds/AdsList/AdsListView.swift
+++ LeboncoinAds/AdsList/AdsListView.swift
@@ -3,6 +3,9 @@ import UIKit
 /// Representation of classified ads
 final class AdsListView: UITableView {
     
+    /// Current reuse identifier of the table's cells
+    var reuseID = ""
+    
     /// Init table with default parameters
     init() {
         super.init(frame: .zero, style: .insetGrouped)
@@ -16,16 +19,14 @@ final class AdsListView: UITableView {
     /// - Parameters:
     ///   - cellClass: The class of a cell that you want to use in the table
     ///   - id: The reuse identifier for the cell. This parameter must not be an empty string
-    ///   - dataSource: The object that acts as the data source of the table view. Not retained
     ///   - delegate: The object that acts as the delegate of the table view. Not retained
     func configure(
         cellClass: UITableViewCell.Type = UITableViewCell.self,
         reuseID id: String = "AdsCell",
-        dataSource: UITableViewDataSource? = nil,
         delegate: UITableViewDelegate? = nil
     ) {
+        reuseID = id
         register(cellClass, forCellReuseIdentifier: id)
-        self.dataSource = dataSource
         self.delegate = delegate
     }
 }
