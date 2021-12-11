ed3174bb15868efac2ce5396ca4f1ba22bf06a9d
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-11 22:00:47 +0100

    Storing selection state in view model

---
+++ Models/Sources/Models/ViewModel/SelectableCategory.swift
@@ -0,0 +1,20 @@
+/// Category which information about selection state
+public struct SelectableCategory {
+    
+    /// Identifier to match with ``AdComplete``
+    public let id: Int
+    
+    /// Readable name of category
+    public let name: String
+    
+    /// Selection state
+    ///
+    /// Initialy false
+    public var isSelected: Bool = true
+
+    public init(id: Int, name: String, isSelected: Bool = true) {
+        self.id = id
+        self.name = name
+        self.isSelected = isSelected
+    }
+}
