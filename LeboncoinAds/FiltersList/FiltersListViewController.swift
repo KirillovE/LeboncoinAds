97f608b837c35ad1fd62d54866c3c2f80d5ea872
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-11 21:37:57 +0100

    Populating table with filters

--- LeboncoinAds/FiltersList/FiltersListViewController.swift
+++ LeboncoinAds/FiltersList/FiltersListViewController.swift
@@ -3,7 +3,10 @@ import Models
 
 final class FiltersListViewController: UIViewController, FilterProvider {
     
+    private let table = FiltersListView()
     private let categories: [Models.Category]
+    private var dataSource: UITableViewDiffableDataSource<Int, Models.Category>?
+    
     var categorySelectionHandler: CategoryInfo?
     
     init(categories: [Models.Category]) {
@@ -17,6 +20,23 @@ final class FiltersListViewController: UIViewController, FilterProvider {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.configure(
+            superview: view,
+            dataSource: &dataSource,
+            delegate: nil
+        )
+        updateUI(animated: false)
     }
 
+}
+
+private extension FiltersListViewController {
+    
+    func updateUI(animated: Bool = true) {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Models.Category>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(categories)
+        dataSource?.apply(snapshot, animatingDifferences: animated)
+    }
+    
 }
