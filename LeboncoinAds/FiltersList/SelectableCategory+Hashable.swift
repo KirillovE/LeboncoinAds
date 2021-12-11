dfa8de4c2e9ea2972cb8d509bd48efbf6f0a9449
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-11 22:52:49 +0100

    Refining identity

--- LeboncoinAds/FiltersList/SelectableCategory+Hashable.swift
+++ LeboncoinAds/FiltersList/SelectableCategory+Hashable.swift
@@ -3,10 +3,11 @@ import Models
 extension SelectableCategory: Hashable {
     
     public static func == (lhs: SelectableCategory, rhs: SelectableCategory) -> Bool {
-        lhs.id == rhs.id
+        lhs.id == rhs.id && lhs.isSelected == rhs.isSelected
     }
     
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+        hasher.combine(isSelected)
     }
 }
