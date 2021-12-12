1ecfb9cc478893824256bcf8f3df441402cacee6
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-12 16:40:34 +0100

    Will present ad using separate sections

---
+++ LeboncoinAds/Details/DetailsSection.swift
@@ -0,0 +1,12 @@
+/// Sections for details representation
+enum DetailsSection: Hashable {
+    
+    /// Section with image(s)
+    case image
+    
+    /// Section with main info, e.g. title, price
+    case mainInfo
+    
+    /// Section with description
+    case description
+}
