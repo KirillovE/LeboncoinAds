8dd30c13480e34f982b9ae454f0f5fb41e897c44
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-12 21:30:19 +0100

    Configuring controller

--- LeboncoinAds/Details/DetailsViewController.swift
+++ LeboncoinAds/Details/DetailsViewController.swift
@@ -1,9 +1,28 @@
 import UIKit
+import Models
 
 final class DetailsViewController: UIViewController {
 
+    private let collection = DetailsView()
+    private var dataSource: UICollectionViewDiffableDataSource<DetailsSection, AdDetails>?
+    private let data: AdDetails
+    
+    init(data: AdDetails) {
+        self.data = data
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collection.configure(
+            data: data,
+            superview: view,
+            dataSource: &dataSource
+        )
     }
 
 }
