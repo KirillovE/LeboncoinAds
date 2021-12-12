13f467f952ca808f04976494cc4a7a9dcfb6ae50
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-12 18:31:02 +0100

    Refining sections

--- LeboncoinAds/Details/DetailsSection.swift
+++ LeboncoinAds/Details/DetailsSection.swift
@@ -1,12 +1,25 @@
+import CoreGraphics
+
 /// Sections for details representation
-enum DetailsSection: Hashable {
+enum DetailsSection: Int, CaseIterable {
     
-    /// Section with image(s)
-    case image
-    
-    /// Section with main info, e.g. title, price
+    /// Section with main info, e.g. title, price, image
     case mainInfo
     
     /// Section with description
     case description
+    
+    /// Column count for different layout widths
+    /// - Parameter width: Current section width
+    /// - Returns: Column count
+    func columnCount(dor width: CGFloat) -> Int {
+        let wideMode = width > 800
+        
+        switch self {
+        case .mainInfo:
+            return wideMode ? 2 : 1
+        case .description:
+            return 1
+        }
+    }
 }
