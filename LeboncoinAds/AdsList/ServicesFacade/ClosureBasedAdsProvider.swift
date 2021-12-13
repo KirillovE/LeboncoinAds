06520a23a2678d7bbabe0caf3e3f333084cf12d1
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-13 19:16:36 +0100

    Providing data

--- LeboncoinAds/AdsList/ServicesFacade/ClosureBasedAdsProvider.swift
+++ LeboncoinAds/AdsList/ServicesFacade/ClosureBasedAdsProvider.swift
@@ -1,13 +1,17 @@
 import Models
+import Foundation
 
 /// Provides actual data using closure syntax
 ///
 /// Interscts with services using closure-like callbacks.
 /// Other providers can utilize ither approaches - Combine, async/await, delegate, ...
-struct ClosureBasedAdsProvider: AdsProvider {
+final class ClosureBasedAdsProvider: AdsProvider {
     
     private let adsLoader: AdsAPIService
     private let modelsConverter: ModelsConverterService
+    private var rawCategories = [Models.Category]()
+    private var rawAds = [ClassifiedAd]()
+    private let group = DispatchGroup()
     
     var adsHandler: AdsInfo?
     var categoriesHandler: CategoriesInfo?
@@ -22,13 +26,55 @@ struct ClosureBasedAdsProvider: AdsProvider {
     }
     
     func fetchAds() {
-        adsLoader.loadCategories { categoryResponse in
+        fetchClassifiedAds()
+        fetchCategories()
+        
+        group.notify(queue: .main) {
+            let adsModels = self.modelsConverter.convert(self.rawAds, using: self.rawCategories)
+            self.adsHandler?(adsModels)
+        }
+    }
+    
+}
+
+private extension ClosureBasedAdsProvider {
+    
+    func fetchClassifiedAds() {
+        group.enter()
+        adsLoader.loadClassifiedAds { [weak self] adsResponse in
+            switch adsResponse {
+            case .success(let ads):
+                self?.rawAds = ads
+            case .failure(let error):
+                self?.handleErrorOnMain(error)
+            }
+            self?.group.leave()
+        }
+    }
+    
+    func fetchCategories() {
+        group.enter()
+        adsLoader.loadCategories { [weak self] categoryResponse in
             switch categoryResponse {
             case .success(let categories):
-                print(categories)
+                self?.rawCategories = categories
+                self?.convertCategories(categories)
             case .failure(let error):
-                print(error)
+                self?.handleErrorOnMain(error)
             }
+            
+            self?.group.leave()
+        }
+    }
+    
+    func convertCategories(_ rawCategories: [Models.Category]) {
+        let categoryModels = modelsConverter.convert(rawCategories)
+        categoriesHandler?(categoryModels)
+    }
+    
+    func handleErrorOnMain(_ error: TextualError) {
+        DispatchQueue.main.async {
+            self.errorHandler?(error)
         }
     }
     
