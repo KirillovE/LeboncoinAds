daecf6970a06964ddf3977cd320f44e77958e1b3
Evgeniy Kirillov <evgeniy.kirillov@appsfactory.de>
2021-12-13 01:25:04 +0100

    Preserving selected state

--- LeboncoinAds/Shared/ListSelectionDelegate.swift
+++ LeboncoinAds/Shared/ListSelectionDelegate.swift
@@ -9,14 +9,12 @@ final class ListSelectionDelegate: NSObject, SelectionProvider {
 
 extension ListSelectionDelegate: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
         selectionHandler?(indexPath)
     }
 }
 
 extension ListSelectionDelegate: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
         selectionHandler?(indexPath)
     }
 }
